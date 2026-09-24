b6cd89952577bfe2ccb1abae62ce0fdbf3e0311c
imac20 <imac20@iMacAdmin20s-iMac.local>
2026-09-24 12:38:16 +0700

    Complete cell04 exercises

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,7 @@
+first_number = int(input("Give me the first number: "))
+second_number = int(input("Give me the second number: "))
+
+print(first_number, "+", second_number, "=", first_number + second_number)
+print(first_number, "-", second_number, "=", first_number - second_number)
+print(first_number, "/", second_number, "=", first_number / second_number)
+print(first_number, "*", second_number, "=", first_number * second_number)
